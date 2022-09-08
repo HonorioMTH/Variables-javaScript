75fc62d348630687f99d4de7d1a949840238c032
HonorioMTH <matheus.honorio88@gmail.com>
2022-09-08 11:54:57 -0300

    Exercicios variaveis

---
+++ index.js
@@ -0,0 +1,13 @@
+let nome, altura, peso, dataDeInternacao, diab
+
+nome = prompt("Seu nome: ");
+altura = prompt("Sua altura: ");
+peso = prompt("Seu peso: ");
+dataDeInternacao = prompt("Sua data de internação: ");
+diab = prompt("É diabético? ");
+
+alert("Nome: " + nome);
+alert("Altura: " + altura);
+alert("Peso: " + peso);
+alert("Data de internação: " + dataDeInternacao);
+alert(" O paciente testou " + diab + " para diabetes")
